0f16db6bf198dfbf58b82d53e71a1676825acf7d
will bai <will.bai.au@gmail.com>
2020-05-29 11:29:19 +0800

    修改client类调用方式
    可以设置连接参数
    可以链式调用

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+### 使用说明
+
+```php
+
+$client = (new MT5Client())->setHost('127.0.0.1')->setPort(9090)->instantiation()->open();
+echo $client->Version();
+$client->close();
+
+```
+
 ### 生成依赖文件
 
 - 生成 php 依赖文件
--- src/MT5Client.php
+++ src/MT5Client.php
@@ -20,41 +20,100 @@ use mt5\MT5ServiceClient;
 
 class MT5Client
 {
-	private $r_buf_size;
-	private $w_buf_size;
-	private $send_time_out;
-	private $recv_time_out;
-	private $transport;
-	private $client;
+    private $transport;
+    private $client;
+    private $host;
+    private $port;
+    private $send_time_out = 60000;
+    private $recv_time_out = 60000;
+    private $r_buf_size = 1024;
+    private $w_buf_size = 1024;
 
     public function __construct()
     {
-        $this->r_buf_size = 1024;
-        $this->w_buf_size = 1024;
-        $this->send_time_out = 60;
-        $this->recv_time_out = 60;
 
-        $socket = new TSocket("127.0.0.1", 9090);
-        $socket->setSendTimeout($this->send_time_out * 1000);
-        $socket->setRecvTimeout($this->recv_time_out * 1000);
+    }
+
+    /**
+     * @param mixed $host
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+        return $this;
+    }
+
+    /**
+     * @param int $port
+     */
+    public function setPort($port)
+    {
+        $this->port = $port;
+        return $this;
+    }
+
+    /**
+     * @param int $send_time_out
+     */
+    public function setSendTimeOut($send_time_out)
+    {
+        $this->send_time_out = $send_time_out;
+        return $this;
+    }
+
+    /**
+     * @param int $recv_time_out
+     */
+    public function setRecvTimeOut($recv_time_out)
+    {
+        $this->recv_time_out = $recv_time_out;
+        return $this;
+    }
+
+    /**
+     * @param int $r_buf_size
+     */
+    public function setRBufSize($r_buf_size)
+    {
+        $this->r_buf_size = $r_buf_size;
+        return $this;
+    }
+
+    /**
+     * @param int $w_buf_size
+     */
+    public function setWBufSize($w_buf_size)
+    {
+        $this->w_buf_size = $w_buf_size;
+        return $this;
+    }
+
+    public function instantiation()
+    {
+        $socket = new TSocket($this->host, $this->port);
+        $socket->setSendTimeout($this->send_time_out);
+        $socket->setRecvTimeout($this->recv_time_out);
 
         $this->transport = new TBufferedTransport($socket, $this->r_buf_size, $this->w_buf_size);
         $protocol = new TBinaryProtocolAccelerated($this->transport);
         $this->client = new MT5ServiceClient($protocol);
-    }
-
-    public function __call($name, $arguments)
-    {
-        return call_user_func_array( array($this->client, $name), $arguments);
+        return $this;
     }
 
     public function open()
     {
         $this->transport->open();
+        return $this;
     }
 
     public function close()
     {
         $this->transport->close();
+        return $this;
+    }
+
+    public function __call($name, $arguments)
+    {
+        return call_user_func_array(array($this->client, $name), $arguments);
     }
 }
